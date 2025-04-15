50e3fd580e66c786122f80f81530c28a11fcda42
VishwajeetRupnawar-169 <142874925+VishwajeetRupnawar-169@users.noreply.github.com>
2025-04-15 21:37:34 +0530

    Changes according to Github Actions PyLint

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
+"""
+demo.py - This module contains a simple hello function for the dev branch.
+"""
+
 def hello():
     """
     This is hello from demo.py
     """
     return print("Hello dev branch !!!")
-
 
 hello()
--- hook_demo.py
+++ hook_demo.py
@@ -1,9 +1,15 @@
+"""
+hook_demo.py - Demonstrates a simple function for addition.
+"""
+
 def my_func():
+    """
+    Adds two numbers and returns the result.
+    """
     print("This is my_func from hook_demo.py")
     a = 10
     b = 20
     d = a + b
     return d
-
 
 my_func()
